e8396add05c5d95addcdda50d6af063e8f622b66
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2022-08-17 14:36:27 +0700

    counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import { useState } from "react";
+import { useReducer } from "react";
 import "./App.css";
 
+const countReducer = (state, action) => {
+  switch (action.type) {
+    case "INCREASE":
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+    case "DECREASE":
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+    default:
+      throw new Error();
+  }
+};
+
 function App() {
-  return;
-  <div></div>;
+  const [state, dispatch] = useReducer(countReducer, { count: 0 });
+
+  const handleClickIncrease = () => {
+    dispatch({ type: "INCREASE" });
+  };
+
+  const handleClickDecrease = () => {
+    dispatch({ type: "DECREASE" });
+  };
+
+  return (
+    <div className="Count">
+      <div className="App">
+        <h1>Hello {state.count}</h1>
+      </div>
+      <div className="button">
+        <button onClick={handleClickDecrease}>-</button>
+        <button onClick={handleClickIncrease}>+</button>
+      </div>
+    </div>
+  );
 }
 export default App;
