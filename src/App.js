e5aeb94790f940c09fe650aaaa710dbad2564d6d
maithanhtrong1113 <maithanhtrong1113@gmail.com>
2022-08-18 15:54:46 +0700

    frist commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./App.css";
 
 function App() {
-  return;
-  <div></div>;
+  const [count, setCount] = useState(60);
+  const ref = useRef(99);
+  const handleStart = () => {
+    ref.current = setInterval(() => {
+      setCount((prev) => prev - 1);
+    }, 1000);
+  };
+
+  const handleStop = () => {
+    clearInterval(ref.current);
+  };
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </div>
+  );
 }
 export default App;
